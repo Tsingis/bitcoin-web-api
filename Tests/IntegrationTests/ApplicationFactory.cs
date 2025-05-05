a24e7c5114c366676e4af2a9d0e8ec3c1a5f3a96
Jesse Kela <jessekkela@gmail.com>
2025-05-05 19:57:15 +0300

    Output server side errors with stack trace in integration testing

--- Tests/IntegrationTests/ApplicationFactory.cs
+++ Tests/IntegrationTests/ApplicationFactory.cs
@@ -1,10 +1,29 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Logging;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Xunit;
+using MartinCostello.Logging.XUnit;
 
 namespace IntegrationTests;
 
-public sealed class ApplicationFactory : WebApplicationFactory<Program>, IAsyncLifetime
+public sealed class ApplicationFactory : WebApplicationFactory<Program>, IAsyncLifetime, ITestOutputHelperAccessor
 {
+
+    public ITestOutputHelper? OutputHelper { get; set; }
+
+    public void ClearOutputHelper()
+        => OutputHelper = null;
+
+    public void SetOutputHelper(ITestOutputHelper? value)
+        => OutputHelper = value;
+
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        builder.ConfigureLogging(loggingBuilder =>
+            loggingBuilder.ClearProviders().AddXUnit(this)
+        );
+    }
+
     public ValueTask InitializeAsync()
     {
         return ValueTask.CompletedTask;
