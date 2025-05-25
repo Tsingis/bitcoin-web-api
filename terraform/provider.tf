088f90de0efbcdd5692e03fb3e879e34e4af2d4c
Jesse Kela <jessekkela@gmail.com>
2025-05-25 19:22:22 +0300

    Terraform version updates

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,13 +1,8 @@
 terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = ">= 4.27.0"
-    }
-  }
-
   backend "azurerm" {
-    key = "terraform.tfstate"
+    use_cli          = true
+    use_azuread_auth = true
+    key              = "terraform.tfstate"
   }
 }
 
