3134d5cd454e7b142879e4b9793d989646d78a47
Jesse Kela <38571453+Tsingis@users.noreply.github.com>
2025-08-30 11:14:20 +0300

    Move from APIWeaver.OpenApi to OpenApi.Extensions (#197)

--- src/Api/Setup/DateOnlyExampleProvider.cs
+++ src/Api/Setup/DateOnlyExampleProvider.cs
@@ -1,8 +1,18 @@
-using APIWeaver;
+
+using System.Text.Json.Serialization;
+using MartinCostello.OpenApi;
 
 namespace Api.Setup;
 
 public sealed class DateOnlyExampleProvider : IExampleProvider<DateOnly>
 {
-    public static DateOnly GetExample() => DateOnly.FromDateTime(DateTime.UtcNow);
+    public static DateOnly GenerateExample() => DateOnly.FromDateTime(DateTime.UtcNow);
 }
+
+[JsonSerializable(typeof(DateOnly))]
+[JsonSourceGenerationOptions(
+    DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull,
+    NumberHandling = JsonNumberHandling.Strict,
+    PropertyNamingPolicy = JsonKnownNamingPolicy.CamelCase,
+    WriteIndented = true)]
+public sealed partial class DateOnlyJsonSerializerContext : JsonSerializerContext;
