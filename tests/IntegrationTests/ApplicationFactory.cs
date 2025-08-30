346d0e4e350cef8adf35c1c26a1f0fb5d0b80e24
Jesse Kela <jessekkela@gmail.com>
2025-08-30 23:36:21 +0300

    Remove obsolete code

--- tests/IntegrationTests/ApplicationFactory.cs
+++ tests/IntegrationTests/ApplicationFactory.cs
@@ -1,17 +1,7 @@
 using Microsoft.AspNetCore.Mvc.Testing;
-using Xunit;
 
 namespace IntegrationTests;
 
-public sealed class ApplicationFactory : WebApplicationFactory<Program>, IAsyncLifetime
+public sealed class ApplicationFactory : WebApplicationFactory<Program>
 {
-    public ValueTask InitializeAsync()
-    {
-        return ValueTask.CompletedTask;
-    }
-
-    public new async ValueTask DisposeAsync()
-    {
-        await base.DisposeAsync().ConfigureAwait(false);
-    }
 }
