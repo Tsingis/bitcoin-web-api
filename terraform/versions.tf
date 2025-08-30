e40a80911e6703008f24e9244ba7cf3c7435c6de
Jesse Kela <38571453+Tsingis@users.noreply.github.com>
2025-08-30 18:42:58 +0300

    Add terraform linting workflow (#200)

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -6,5 +6,10 @@ terraform {
       source  = "hashicorp/azurerm"
       version = ">= 4.42.0"
     }
+
+    external = {
+      source  = "hashicorp/external"
+      version = ">= 2.3.5"
+    }
   }
 }
