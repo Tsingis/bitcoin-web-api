c5be00cb6a6524b88c005513fc32f7480d5b2930
Jesse Kela <jessekkela@gmail.com>
2026-01-18 22:15:01 +0200

    Testing rate limits and output caching

---
+++ tests/IntegrationTests/InfraBehavarioTests.cs
@@ -0,0 +1,46 @@
+using System.Net;
+using IntegrationTests.Setup;
+using Shouldly;
+using Xunit;
+
+namespace IntegrationTests;
+
+public sealed class InfraBehaviorTests(Fixture fixture)
+{
+    [Theory]
+    [InlineData(false, false)]
+    [InlineData(true, true)]
+    public async Task Hits_Cache(bool useOutputCache, bool expectedCacheHit)
+    {
+        using var factory = new TestApplicationFactory(fixture, useOutputCache);
+        var client = factory.CreateClient();
+
+        var ct = TestContext.Current.CancellationToken;
+        var url = new Uri($"{Constants.BaseUrl}/buyandsell?fromDate={Constants.s_mockDate}&toDate={Constants.s_mockDate.AddDays(11)}", UriKind.Relative);
+        var first = await client.GetAsync(url, cancellationToken: ct);
+        var second = await client.GetAsync(url, cancellationToken: ct);
+
+        first.Headers.Select(x => x.Key).ShouldNotContain("Age");
+        second.Headers.Select(x => x.Key).Contains("Age").ShouldBe(expectedCacheHit);
+    }
+
+    [Theory]
+    [InlineData(false, HttpStatusCode.TooManyRequests)]
+    [InlineData(true, HttpStatusCode.OK)]
+    public async Task Hits_RateLimit(bool useOutputCache, HttpStatusCode expectedStatusCode)
+    {
+        using var factory = new TestApplicationFactory(fixture, useOutputCache);
+        var client = factory.CreateClient();
+
+        var ct = TestContext.Current.CancellationToken;
+        var url = new Uri($"{Constants.BaseUrl}/buyandsell?fromDate={Constants.s_mockDate}&toDate={Constants.s_mockDate.AddDays(11)}", UriKind.Relative);
+
+        HttpResponseMessage? result = null;
+        for (var i = 0; i < 10; i++)
+        {
+            result = await client.GetAsync(url, cancellationToken: ct);
+        }
+
+        result?.StatusCode.ShouldBe(expectedStatusCode);
+    }
+}
