5ccc2b8ff5f39b5c1196224048552c53c8cb71a2
Jesse Kela <38571453+Tsingis@users.noreply.github.com>
2026-01-18 16:18:34 +0200

    xUnit with mtp v2 (#278)

--- coverage.ps1
+++ coverage.ps1
@@ -1,5 +1,6 @@
 dotnet build -c Release
-dotnet test -c Release --no-build --settings tests/coverage.runsettings --results-directory test-results
+dotnet test --project tests/UnitTests -c Release --no-build --results-directory test-results `
+--coverage --coverage-output-format cobertura --coverage-output coverage.cobertura.xml
 
 $coverageFiles = Get-ChildItem -Path "test-results" -Recurse -Filter "*cobertura.xml"
 
