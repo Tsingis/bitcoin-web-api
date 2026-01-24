1289707e1284e074b5cb74c5d107ed0207ec454e
Jesse Kela <jessekkela@gmail.com>
2026-01-24 17:11:06 +0200

    Test endspoints have expected responses

--- tests/IntegrationTests/OpenApiDocsTests.cs
+++ tests/IntegrationTests/OpenApiDocsTests.cs
@@ -8,26 +8,26 @@ namespace IntegrationTests;
 
 public sealed class OpenApiDocsTests(WiremockFixture fixture, ITestOutputHelper outputHelper) : TestBase(fixture, outputHelper)
 {
+    public const string DocsUrl = "/openapi/v1.json";
+
     [Fact]
     public async Task DocsHasCustomExamples()
     {
         var ct = TestContext.Current.CancellationToken;
-        var result = await _client.GetAsync(new Uri("/openapi/v1.json", UriKind.Relative), cancellationToken: ct);
+        var result = await _client.GetAsync(new Uri(DocsUrl, UriKind.Relative), ct);
         result.StatusCode.ShouldBe(HttpStatusCode.OK);
 
-        var data = await result.Content.ReadAsStringAsync(cancellationToken: ct);
+        var data = await result.Content.ReadAsStringAsync(ct);
 
         Should.NotThrow(() =>
         {
             using var document = JsonDocument.Parse(data);
             var root = document.RootElement;
 
-            root.TryGetProperty("openapi", out var _).ShouldBeTrue("Missing 'openapi' key");
+            root.TryGetProperty("openapi", out var _).ShouldBeTrue("Missing 'openapi'");
 
             var firstPath = root.GetProperty("paths").EnumerateObject().First();
-            var parameters = firstPath.Value
-                .GetProperty("get")
-                .GetProperty("parameters");
+            var parameters = firstPath.Value.GetProperty("get").GetProperty("parameters");
 
             foreach (var param in parameters.EnumerateArray())
             {
@@ -43,8 +43,53 @@ public sealed class OpenApiDocsTests(WiremockFixture fixture, ITestOutputHelper 
                     example = schemaExample.GetString();
                 }
 
-                example.ShouldNotBeNullOrWhiteSpace("Missing example for parameter");
+                example.ShouldNotBeNullOrWhiteSpace($"Missing example for parameter {param}");
             }
         });
     }
+
+    [Fact]
+    public async Task AllEndpointsHaveExpectedResponses()
+    {
+        var ct = TestContext.Current.CancellationToken;
+        var result = await _client.GetAsync(new Uri(DocsUrl, UriKind.Relative), ct);
+        result.StatusCode.ShouldBe(HttpStatusCode.OK);
+
+        var data = await result.Content.ReadAsStringAsync(ct);
+
+        using var document = JsonDocument.Parse(data);
+        var root = document.RootElement;
+
+        root.TryGetProperty("paths", out var paths).ShouldBeTrue("Missing 'paths'");
+
+        var expectedStatusCodes = new[]
+        {
+            HttpStatusCode.OK,
+            HttpStatusCode.NoContent,
+            HttpStatusCode.BadRequest,
+            HttpStatusCode.Unauthorized,
+            HttpStatusCode.TooManyRequests,
+            HttpStatusCode.InternalServerError
+        }.Cast<int>().ToArray();
+
+        foreach (var path in paths.EnumerateObject())
+        {
+            foreach (var operation in path.Value.EnumerateObject())
+            {
+                var method = operation.Name;
+
+                operation.Value.TryGetProperty("responses", out var responses)
+                    .ShouldBeTrue($"Missing 'responses' for {method.ToUpper()} {path.Name}");
+
+                var actualResponseCodes = responses
+                    .EnumerateObject()
+                    .Select(x => int.Parse(x.Name))
+                    .OrderBy(x => x)
+                    .ToArray();
+
+                actualResponseCodes.ShouldBe(expectedStatusCodes, $"Unexpected responses for {method.ToUpper()} {path.Name}");
+            }
+        }
+    }
+
 }
