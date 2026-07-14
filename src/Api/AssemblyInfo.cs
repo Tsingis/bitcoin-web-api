14b911a5b870a4de1b22ad7c52417594bca590ad
Jesse Kela <jessekkela@gmail.com>
2026-07-14 23:09:05 +0300

    Declare InternalsVisibleTo properties in project file

--- src/Api/AssemblyInfo.cs
+++
@@ -1,5 +0,0 @@
-using System.Runtime.CompilerServices;
-
-[assembly: InternalsVisibleTo("ArchitectureTests")]
-[assembly: InternalsVisibleTo("IntegrationTests")]
-[assembly: InternalsVisibleTo("E2ETests")]
