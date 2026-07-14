db9448d3f4256a0ac34b702c0aadd305048d5147
Jesse Kela <jessekkela@gmail.com>
2026-07-14 23:04:32 +0300

    API internals visible to integration tests

--- src/Api/AssemblyInfo.cs
+++ src/Api/AssemblyInfo.cs
@@ -1,4 +1,5 @@
 using System.Runtime.CompilerServices;
 
 [assembly: InternalsVisibleTo("ArchitectureTests")]
+[assembly: InternalsVisibleTo("IntegrationTests")]
 [assembly: InternalsVisibleTo("E2ETests")]
